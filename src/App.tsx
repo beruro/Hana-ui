96519d15face73ea3d25df1552f4235da27e4aa0
Fish <junyu.wei@foxmail.com>
2023-05-11 22:50:27 +0800

    icon part_one

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,25 @@
 // export { default as Button } from "./components/Button";
 import React from "react";
-
+// import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+// import { faCoffee } from "@fortawesome/free-solid-svg-icons";
+import { library } from "@fortawesome/fontawesome-svg-core";
+import {
+  faCheckSquare,
+  faCoffee,
+  fas,
+} from "@fortawesome/free-solid-svg-icons";
 import Button from "./components/Button/button";
 import Menu from "./components/Menu/menu";
 import MenuItem from "./components/Menu/menuItem";
 import SubMenu from "./components/Menu/subMenu";
-
+import Icon from "./components/Icon/icon";
+library.add(fas);
 function App() {
   return (
     <div className="App">
       <header className="App-header">
+        <Icon icon="arrow-up" theme="danger" size="10x" />
+        {/* <FontAwesomeIcon icon={faCoffee} size="10x" /> */}
         <Menu
           defaultIndex="0"
           onSelect={(index) => {
