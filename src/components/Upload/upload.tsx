703312e34db431e7576909fd6badd1102c21baca
Fish <junyu.wei@foxmail.com>
2023-05-15 13:16:16 +0800

    upload modify

--- src/components/Upload/upload.tsx
+++ src/components/Upload/upload.tsx
@@ -1,24 +1,95 @@
 import React, { FC, useRef, ChangeEvent, useState } from "react";
 import axios from "axios";
-import Button from "../Button";
-import Input from "../Input";
-
+import UploadList from "./uploadList";
+import Dragger from "./dragger";
+export type UploadFileStatus = "ready" | "uploading" | "success" | "error";
+export interface UploadFile {
+  uid: string;
+  size: number;
+  name: string;
+  status?: UploadFileStatus;
+  percent: number;
+  raw?: File;
+  response?: any;
+  error?: any;
+}
 export interface UploadProps {
+  /**必选参数, 上传的地址 */
   action: string;
+  /**上传的文件列表,*/
+  defaultFileList?: UploadFile[];
   /**上传文件之前的钩子，参数为上传的文件，若返回 false 或者 Promise 则停止上传。 */
   beforeUpload?: (file: File) => boolean | Promise<File>;
-  onProgress?: (percentage: number, File: File) => void;
-  onSuccess?: (data: any, file: File) => void;
-  onError?: (err: any, file: File) => void;
+  /**文件上传时的钩子 */
+  onProgress?: (percentage: number, file: UploadFile) => void;
+  /**文件上传成功时的钩子 */
+  onSuccess?: (data: any, file: UploadFile) => void;
+  /**文件上传失败时的钩子 */
+  onError?: (err: any, file: UploadFile) => void;
   /**文件状态改变时的钩子，上传成功或者失败时都会被调用	 */
-  onChange?: (file: File) => void;
+  onChange?: (file: UploadFile) => void;
+  /**文件列表移除文件时的钩子 */
+  onRemove?: (file: UploadFile) => void;
+  /**设置上传的请求头部 */
+  headers?: { [key: string]: any };
+  /**上传的文件字段名 */
+  name?: string;
+  /**上传时附带的额外参数 */
+  data?: { [key: string]: any };
+  /**支持发送 cookie 凭证信息 */
+  withCredentials?: boolean;
+  /**可选参数, 接受上传的文件类型 */
+  accept?: string;
+  /**是否支持多选文件 */
+  multiple?: boolean;
+  /**是否支持拖拽上传 */
+  drag?: boolean;
   children?: React.ReactNode;
 }
 
+/**
+ * 通过点击或者拖拽上传文件
+ * ### 引用方法
+ *
+ * ~~~js
+ * import { Upload } from 'fishship'
+ * ~~~
+ */
 export const Upload: FC<UploadProps> = (props) => {
-  const { action, onProgress, onError, onSuccess, beforeUpload, onChange } =
-    props;
+  const {
+    action,
+    defaultFileList,
+    beforeUpload,
+    onProgress,
+    onSuccess,
+    onError,
+    onChange,
+    onRemove,
+    name,
+    headers,
+    data,
+    withCredentials,
+    accept,
+    multiple,
+    children,
+    drag,
+  } = props;
   const fileInput = useRef<HTMLInputElement>(null);
+  const [fileList, setFileList] = useState<UploadFile[]>(defaultFileList || []);
+  const updateFileList = (
+    updateFile: UploadFile,
+    updateObj: Partial<UploadFile>
+  ) => {
+    setFileList((prevList) => {
+      return prevList.map((file) => {
+        if (file.uid === updateFile.uid) {
+          return { ...file, ...updateObj };
+        } else {
+          return file;
+        }
+      });
+    });
+  };
   const handleClick = () => {
     if (fileInput.current) {
       fileInput.current.click();
@@ -32,6 +103,14 @@ export const Upload: FC<UploadProps> = (props) => {
     uploadFiles(files);
     if (fileInput.current) {
       fileInput.current.value = "";
+    }
+  };
+  const handleRemove = (file: UploadFile) => {
+    setFileList((prevList) => {
+      return prevList.filter((item) => item.uid !== file.uid);
+    });
+    if (onRemove) {
+      onRemove(file);
     }
   };
   const uploadFiles = (files: FileList, test?: boolean) => {
@@ -55,58 +134,103 @@ export const Upload: FC<UploadProps> = (props) => {
     });
   };
   const post = (file: File) => {
+    let _file: UploadFile = {
+      uid: Date.now() + "upload-file",
+      status: "ready",
+      name: file.name,
+      size: file.size,
+      percent: 0,
+      raw: file,
+    };
+    setFileList((prevList) => {
+      return [_file, ...prevList];
+    });
     const formData = new FormData();
-    formData.append(file.name, file);
+    formData.append(name || "file", file);
+    if (data) {
+      Object.keys(data).forEach((key) => {
+        formData.append(key, data[key]);
+      });
+    }
     axios
       .post(action, formData, {
         headers: {
+          ...headers,
           "Content-Type": "multipart/form-data",
         },
-
+        withCredentials,
         onUploadProgress: (e) => {
-          let percentage = Math.round(
-            (e.loaded * 100) / (e.total as number) || 0
-          );
+          let percentage =
+            Math.round((e.loaded * 100) / (e.total as number)) || 0;
           if (percentage < 100) {
+            updateFileList(_file, { percent: percentage, status: "uploading" });
+            _file.status = "uploading";
+            _file.percent = percentage;
             if (onProgress) {
-              onProgress(percentage, file);
+              onProgress(percentage, _file);
             }
           }
         },
       })
       .then((resp) => {
-        console.log(resp);
+        updateFileList(_file, { status: "success", response: resp.data });
+        _file.status = "success";
+        _file.response = resp.data;
         if (onSuccess) {
-          onSuccess(resp.data, file);
+          onSuccess(resp.data, _file);
         }
         if (onChange) {
-          onChange(file);
+          onChange(_file);
         }
       })
       .catch((err) => {
+        updateFileList(_file, { status: "error", error: err });
+        _file.status = "error";
+        _file.error = err;
         if (onError) {
-          onError(err, file);
+          onError(err, _file);
         }
         if (onChange) {
-          onChange(file);
+          onChange(_file);
         }
       });
   };
+
   return (
     <div className="fish-upload-component">
-      <Button btnType="primary">Upload File</Button>
-      <Input
-        className="fish-file-input"
-        style={{ display: "none" }}
-        type="file"
-        ref={fileInput}
-        onChange={handleFileChange}
-      />
+      <div
+        className="fish-upload-input"
+        style={{ display: "inline-block" }}
+        onClick={handleClick}
+      >
+        {drag ? (
+          <Dragger
+            onFile={(files) => {
+              uploadFiles(files, true);
+            }}
+          >
+            {children}
+          </Dragger>
+        ) : (
+          children
+        )}
+        <input
+          className="fish-file-input"
+          style={{ display: "none" }}
+          ref={fileInput}
+          onChange={handleFileChange}
+          type="file"
+          accept={accept}
+          multiple={multiple}
+        />
+      </div>
+
+      <UploadList fileList={fileList} onRemove={handleRemove} />
     </div>
   );
 };
 
-// Upload.defaultProps = {
-//   name: 'file'
-// }
+Upload.defaultProps = {
+  name: "file",
+};
 export default Upload;
