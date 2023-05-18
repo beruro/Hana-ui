8ea43f2c84380e42419dbd794e11e16c6ceb2fa1
Fish <junyu.wei@foxmail.com>
2023-05-18 13:12:31 +0800

    unknow

---
+++ src/components/Tabs/tabs.stories.tsx
@@ -0,0 +1,46 @@
+import React from "react";
+import { StoryFn, Meta } from "@storybook/react";
+
+import Tabs from "./tabs";
+import TabItem from "./tabItem";
+import Icon from "../Icon";
+export default {
+  title: "第六章作业：Tabs",
+  id: "Tabs",
+  component: Tabs,
+  subcomponents: { TabItem: TabItem },
+} as Meta<typeof Tabs>;
+
+export const ADefaultTabs: StoryFn<typeof Tabs> = (args) => (
+  <Tabs {...args}>
+    <TabItem label="选项卡一">this is content one</TabItem>
+    <TabItem label="选项卡二">this is content two</TabItem>
+    <TabItem label="用户管理">this is content three</TabItem>
+  </Tabs>
+);
+ADefaultTabs.storyName = "默认的Tabs";
+export const BCardTabs: StoryFn<typeof Tabs> = (args) => (
+  <Tabs {...args} type="card">
+    <TabItem label="card1">this is card one</TabItem>
+    <TabItem label="card2">this is content two</TabItem>
+    <TabItem label="disabled" disabled>
+      this is content three
+    </TabItem>
+  </Tabs>
+);
+BCardTabs.storyName = "选项卡样式的Tabs";
+export const CCustomTabs: StoryFn<typeof Tabs> = (args) => (
+  <Tabs {...args} type="card">
+    <TabItem
+      label={
+        <>
+          <Icon icon="check-circle" /> 自定义图标
+        </>
+      }
+    >
+      this is card one
+    </TabItem>
+    <TabItem label="tab2">this is content two</TabItem>
+  </Tabs>
+);
+CCustomTabs.storyName = "自定义选项卡样式";
