39ee6dfbdf759c82c86cec097cc19bc5192ade54
Fish <junyu.wei@foxmail.com>
2023-05-18 12:44:54 +0800

    tabs tsx

--- src/components/Tabs/tabs.tsx
+++ src/components/Tabs/tabs.tsx
@@ -17,3 +17,73 @@ export interface TabsProps {
   type?: "line" | "card";
   children?: ReactNode;
 }
+
+/**
+ * 选项卡切换组件。
+ * 提供平级的区域将大块内容进行收纳和展现，保持界面整洁。
+ * ### 引用方法
+ *
+ * ~~~js
+ * import { Tabs } from 'vikingship'
+ * ~~~
+ */
+export const Tabs: FC<TabsProps> = (props) => {
+  const { defaultIndex, className, onSelect, children, type } = props;
+  const [activeIndex, setActiveIndex] = useState(defaultIndex);
+  const handleClick = (
+    e: React.MouseEvent,
+    index: number,
+    disabled: boolean | undefined
+  ) => {
+    if (!disabled) {
+      setActiveIndex(index);
+      if (onSelect) {
+        onSelect(index);
+      }
+    }
+  };
+  const navClass = classNames("viking-tabs-nav", {
+    "nav-line": type === "line",
+    "nav-card": type === "card",
+  });
+  const renderNavLinks = () => {
+    return React.Children.map(children, (child, index) => {
+      const childElement = child as FunctionComponentElement<TabItemProps>;
+      const { label, disabled } = childElement.props;
+      const classes = classNames("viking-tabs-nav-item", {
+        "is-active": activeIndex === index,
+        disabled: disabled,
+      });
+      return (
+        <li
+          className={classes}
+          key={`nav-item-${index}`}
+          onClick={(e) => {
+            handleClick(e, index, disabled);
+          }}
+        >
+          {label}
+        </li>
+      );
+    });
+  };
+  const renderContent = () => {
+    return React.Children.map(children, (child, index) => {
+      if (index === activeIndex) {
+        return child;
+      }
+    });
+  };
+  return (
+    <div className={`viking-tabs ${className}`}>
+      <ul className={navClass}>{renderNavLinks()}</ul>
+      <div className="viking-tabs-content">{renderContent()}</div>
+    </div>
+  );
+};
+
+Tabs.defaultProps = {
+  defaultIndex: 0,
+  type: "line",
+};
+export default Tabs;
