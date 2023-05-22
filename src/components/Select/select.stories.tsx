42fd90f0677813affc983e832623aede6f85259e
Fish <junyu.wei@foxmail.com>
2023-05-22 16:08:29 +0800

    select story

---
+++ src/components/Select/select.stories.tsx
@@ -0,0 +1,46 @@
+import React from "react";
+import { Meta } from "@storybook/react";
+
+import Select from "./index";
+export default {
+  title: "第九章作业：Select",
+  component: Select,
+  id: "Select",
+  subcomponents: { Option: Select.Option },
+  decorators: [
+    (Story: any) => (
+      <div style={{ width: "350px" }}>
+        <Story />
+      </div>
+    ),
+  ],
+} as Meta<typeof Select>;
+
+export const ADefaultSelect = (args: any) => (
+  <Select {...args} placeholder="请选择">
+    <Select.Option value="nihao" />
+    <Select.Option value="nihao2" />
+    <Select.Option value="nihao3" />
+    <Select.Option value="disabled" disabled />
+    <Select.Option value="nihao5" />
+  </Select>
+);
+// ADefaultSelect.storyName = '默认的Select'
+export const BMultipleSelect = (args: any) => (
+  <Select {...args} placeholder="支持多选欧！" multiple>
+    <Select.Option value="nihao" />
+    <Select.Option value="nihao2" />
+    <Select.Option value="nihao3" />
+    <Select.Option value="viking" />
+    <Select.Option value="viking2" />
+  </Select>
+);
+BMultipleSelect.storyName = "支持多选的 Select";
+export const CDisabledSelect = (args: any) => (
+  <Select {...args} placeholder="禁用啦！" disabled>
+    <Select.Option value="nihao" />
+    <Select.Option value="nihao2" />
+    <Select.Option value="nihao3" />
+  </Select>
+);
+CDisabledSelect.storyName = "被禁用的 Select";
